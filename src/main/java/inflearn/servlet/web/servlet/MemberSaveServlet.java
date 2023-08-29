1d66b8a06ce945ed17a342d4bd5ab49273d3cd45
EUIJOON <euijoonism@gmail.com>
2023-08-30 00:16:36 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/inflearn/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,51 @@
+package inflearn.servlet.web.servlet;
+
+import inflearn.servlet.domain.Member;
+import inflearn.servlet.domain.MemberRepository;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        System.out.println("MemberSaveServlet.service");
+
+        // 폼에서 입력되어 온 것을 파라미터로 꺼낸다.
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        // 저장시킴
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        // 저장 잘 되었는지 응답을 HTML로 내리기
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "    <li>id="+member.getId()+"</li>\n" +
+                "    <li>username="+member.getUsername()+"</li>\n" +
+                "    <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
