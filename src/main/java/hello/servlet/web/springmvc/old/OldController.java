a25b8f273d5991961075c8b5a1c96cc1a513e052
EUIJOON <euijoonism@gmail.com>
2023-09-03 00:35:16 +0900

    뷰 리졸버

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -13,6 +13,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return new ModelAndView("new-form");
+        //return null;
+        return new ModelAndView("new-form"); // 아 아까 이래서 오류페이지가 나온 것이었음. 뷰를 못 찾기 때문에
     }
 }
